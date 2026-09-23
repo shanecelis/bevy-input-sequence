68709541c97d96390dedc99123aa20890a5972dc
Shane Celis <shane.celis@gmail.com>
2026-09-23 04:09:17 -0400

    chore: Update html root.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/bevy-input-sequence/0.10.0")]
+#![doc(html_root_url = "https://docs.rs/bevy-input-sequence/0.11.0")]
 #![doc = include_str!("../README.md")]
 #![forbid(missing_docs)]
 
